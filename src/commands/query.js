45e34ac5f6de203a5c27e941a5be53f5f13b1770
liuxiaodong <1119441533@qq.com>
2021-05-19 23:51:52 +0800

    [Up] 查询命令

--- src/commands/query.js
+++ src/commands/query.js
@@ -1,19 +1,51 @@
-const inquirer = require('inquirer');
-const autocomplete = require('inquirer-autocomplete-prompt');
+const chalk = require('chalk');
+const Table = require('cli-table3');
 
 const {
-  promptPromise,
   handleException,
 } = require('../helper');
 
 const openDB = require('../db/dao');
 
-// 问题
-const {
+let sceneMap = new Map();
+let tplList;
 
-} = require('../config/questions');
+async function query() {
+
+  let res = await openDB.query({}, {
+    sceneType: 1,
+  });
+
+  if (res.state === 'success') {
+
+    res.data.filter(item => item.type && item.type >= 1).forEach(item => {
+      sceneMap.set(item.type, item.name);
+    });
+
+    tplList = res.data.filter(item => item.sceneType && item.sceneType >= 1);
+
+  } else {
+    handleException();
+  }
+
+}
 
 async function handleQueryAction() {
+
+  await query();
+
+  let table = new Table({
+    head: ['应用场景', '技术栈', '描述', '安装操作或下载链接', '预安装操作'],
+    colWidths: [20, 20, 45, 45, 40],
+  });
+
+  let arr = tplList.map(item => {
+    return [sceneMap.get(item.sceneType), item.name, item.desc, item.install, item.preInstall];
+  });
+
+  table.push(...arr);
+
+  console.log(chalk.hex('#3399FF')(table.toString()));
 
 }
 
